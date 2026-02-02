bf2536c90e3882e83646f6c47ab2dbd152d85d02
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2026-02-02 12:41:12 -0300

    Struct experimental para começar algo de fato

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+struct Tarefa{
+    ativo: bool,
+    titulo: String,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+
+
+    let teste = Tarefa {
+        ativo: true,
+        titulo: String::from("Título Experimental"),
+    };
+
+    println!("Ativo: {}\nTítulo: {}", teste.ativo, teste.titulo);
     //OBS.: isso é apenas um projeto de aquecimento.
 }
