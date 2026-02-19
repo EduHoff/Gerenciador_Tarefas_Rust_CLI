52baa6df283d283860f17186c608db0f2d6c5610
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2026-02-19 11:38:35 -0300

    add primeira estrutura para ter um menu

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{self, Write};
+
 struct Tarefa{
     ativo: bool,
     titulo: String,
@@ -11,6 +13,20 @@ fn main() {
         ativo: true,
         titulo: String::from("Título Experimental"),
     };
+
+    loop {
+        println!("--Conceito Menu Interativo--");
+        println!("Digite EOF para sair do loop!");
+        print!("||");
+        io::stdout().flush().expect("Falha ao limpar o buffer");
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Falha ao ler a linha");
+
+        if input.trim() == "EOF" {
+            break;
+        }
+    }
 
     println!("Ativo: {}\nTítulo: {}", teste.ativo, teste.titulo);
     //OBS.: isso é apenas um projeto de aquecimento.
